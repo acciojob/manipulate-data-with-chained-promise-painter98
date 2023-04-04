a8f2c93a2d3998444edf5c119560a4043f325a44
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-04 14:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let arr = [1,2,3,4];
+
+	let odd = arr.filter((value) => {
+		return value%2==1;
+	});
+	let even = arr.filter((value) => {
+		return value%2==0;
+	});
+    let multi = even.map((value) => {
+		return value*2;
+	});
+
+	let promise1 = new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve(odd);
+		},1000);
+	});
+	let promise2 = new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve(multi);
+		},2000);
+	});
+}
+let output =document.getElementById('output');
+
+promise1.then((data) => {
+	output.innerText = data;
+});
+
+promise2.then((data) => {
+	let result = output.innerText;
+	output.innerText = result + data;
+});
